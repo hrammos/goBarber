d70fac863306ab80b7b50cd1d75d290f0af10f9f
Henrique Ramos <henriqueramos0106@gmail.com>
2020-06-19 21:59:39 -0300

    Corretion in the Button component and added Yup

--- frontend/src/components/Button/index.tsx
+++ frontend/src/components/Button/index.tsx
@@ -4,7 +4,7 @@ import { Container } from './styles';
 
 type ButtonProps = ButtonHTMLAttributes<HTMLButtonElement>;
 
-const Button: React.FC<ButtonProps> = ({ children }, ...rest) => (
+const Button: React.FC<ButtonProps> = ({ children, ...rest }) => (
   <Container type="button" {...rest}>
     {children}
   </Container>
