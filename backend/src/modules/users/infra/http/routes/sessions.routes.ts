e7e099fe16318775a6b4d62d6e2ba5d845247df7
Henrique Ramos <henriqueramos0106@gmail.com>
2020-07-12 19:50:18 -0300

    create controllers and refactor routes

--- backend/src/modules/users/infra/http/routes/sessions.routes.ts
+++ backend/src/modules/users/infra/http/routes/sessions.routes.ts
@@ -1,21 +1,10 @@
 import { Router } from 'express';
-import { container } from 'tsyringe';
 
-import AuthenticateUserSessionService from '@modules/users/services/AuthenticateUserSessionService';
+import SessionsController from '../controllers/SessionsController';
 
 const sessionsRouter = Router();
+const sessionsController = new SessionsController();
 
-sessionsRouter.post('/', async (request, response) => {
-  const { email, password } = request.body;
-
-  const authenticateUser = container.resolve(AuthenticateUserSessionService);
-
-  const { user, token } = await authenticateUser.execute({
-    email,
-    password,
-  });
-
-  return response.json({ user, token });
-});
+sessionsRouter.post('/', sessionsController.create);
 
 export default sessionsRouter;
