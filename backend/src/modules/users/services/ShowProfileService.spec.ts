2bcdfdc6783f325766dbd89dce02ad5d72875142
Henrique Ramos <henriqueramos0106@gmail.com>
2020-07-21 21:41:49 -0300

    List of providers

--- backend/src/modules/users/services/ShowProfileService.spec.ts
+++ backend/src/modules/users/services/ShowProfileService.spec.ts
@@ -12,7 +12,7 @@ describe('ShowProfile', () => {
     showProfile = new ShowProfileService(fakeUsersRepository);
   });
 
-  it('should be able show the profile', async () => {
+  it('should be able to show the profile', async () => {
     const user = await fakeUsersRepository.create({
       name: 'John Doe',
       email: 'johndoe@example.com',
@@ -27,7 +27,7 @@ describe('ShowProfile', () => {
     expect(profile.email).toBe('johndoe@example.com');
   });
 
-  it('should not be able show the profile from non-existing user', async () => {
+  it('should not be able to show the profile from non-existing user', async () => {
     await expect(
       showProfile.execute({
         user_id: 'non-existing-user-id',
