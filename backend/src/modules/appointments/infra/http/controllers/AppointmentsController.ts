a2f0860afa71b6686c65d4efea2faeecccf21cab
Henrique Ramos <henriqueramos0106@gmail.com>
2020-07-26 21:28:47 -0300

    Added cache and limit requests per second

--- backend/src/modules/appointments/infra/http/controllers/AppointmentsController.ts
+++ backend/src/modules/appointments/infra/http/controllers/AppointmentsController.ts
@@ -1,4 +1,3 @@
-import { parseISO } from 'date-fns';
 import { container } from 'tsyringe';
 import { Request, Response } from 'express';
 
@@ -9,12 +8,10 @@ export default class AppointentsController {
     const user_id = request.user.id;
     const { provider_id, date } = request.body;
 
-    const parsedDate = parseISO(date);
-
     const createAppointment = container.resolve(CreateAppointmentService);
 
     const appointment = await createAppointment.execute({
-      date: parsedDate,
+      date,
       user_id,
       provider_id,
     });
--- backend/src/modules/users/services/AuthenticateUserSession.spec.ts
+++ backend/src/modules/users/services/AuthenticateUserSession.spec.ts
@@ -3,18 +3,16 @@ import AppError from '@shared/errors/AppError';
 import FakeUsersRepository from '../repositories/fakes/FakeUsersRepository';
 import FakeHashProvider from '../providers/HashProvider/fakes/FakeHashProvider';
 import AuthenticateUserSessionService from './AuthenticateUserSessionService';
-import CreateUserService from './CreateUserService';
 
 let fakeUsersRepository: FakeUsersRepository;
 let fakeHashProvider: FakeHashProvider;
-let createUser: CreateUserService;
 let authenticateUser: AuthenticateUserSessionService;
 
 describe('AuthenticateUserSession', () => {
   beforeEach(() => {
     fakeUsersRepository = new FakeUsersRepository();
     fakeHashProvider = new FakeHashProvider();
-    createUser = new CreateUserService(fakeUsersRepository, fakeHashProvider);
+
     authenticateUser = new AuthenticateUserSessionService(
       fakeUsersRepository,
       fakeHashProvider,
@@ -22,7 +20,7 @@ describe('AuthenticateUserSession', () => {
   });
 
   it('should be able to authenticate', async () => {
-    const user = await createUser.execute({
+    const user = await fakeUsersRepository.create({
       name: 'John Doe',
       email: 'johndoe@example.com',
       password: '123456',
@@ -47,7 +45,7 @@ describe('AuthenticateUserSession', () => {
   });
 
   it('should not be able to authenticate with wrong password', async () => {
-    await createUser.execute({
+    await fakeUsersRepository.create({
       name: 'John Doe',
       email: 'johndoe@example.com',
       password: '123456',
