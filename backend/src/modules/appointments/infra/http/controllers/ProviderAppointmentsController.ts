c84435e8b7597f4e55455ebfd12806c9dca78bed
Henrique Ramos <henriqueramos0106@gmail.com>
2020-08-04 22:21:58 -0300

    Adjustment in the list of providers

--- backend/src/modules/appointments/infra/http/controllers/ProviderAppointmentsController.ts
+++ backend/src/modules/appointments/infra/http/controllers/ProviderAppointmentsController.ts
@@ -1,8 +1,8 @@
 import { container } from 'tsyringe';
 import { Request, Response } from 'express';
+import { classToClass } from 'class-transformer';
 
 import ListProviderAppointmentsService from '@modules/appointments/services/ListProviderAppointmentsService';
-import { classToClass } from 'class-transformer';
 
 export default class ProviderAppointmentsController {
   public async index(request: Request, response: Response): Promise<Response> {
