81466d06a7d01deacc273ac1f5d5fe556b080ec3
Henrique Ramos <henriqueramos0106@gmail.com>
2020-08-05 23:03:30 -0300

    Starting mobile dashboard

--- mobile/src/pages/Dashboard/index.tsx
+++ mobile/src/pages/Dashboard/index.tsx
@@ -1,16 +1,65 @@
-import React from 'react';
-import { Button } from 'react-native';
+import React, { useCallback, useEffect, useState } from 'react';
 
+import { useNavigation } from '@react-navigation/native';
 import { useAuth } from '../../hooks/auth';
 
-import { Container } from './styles';
+import {
+  Container,
+  Header,
+  HeaderTitle,
+  UserName,
+  ProfileButton,
+  UserAvatar,
+  ProvidersList,
+} from './styles';
+
+import api from '../../services/api';
+
+export interface Provider {
+  id: string;
+  nome: string;
+  avatar_url: string;
+}
 
 const Dashboard: React.FC = () => {
-  const { signOut } = useAuth();
+  const [providers, setProviders] = useState<Provider[]>([]);
+
+  const { signOut, user } = useAuth();
+  const { navigate } = useNavigation();
+
+  useEffect(() => {
+    async function loadProviders(): Promise<void> {
+      const response = await api.get('/providers');
+
+      setProviders(response.data);
+    }
+
+    loadProviders();
+  }, []);
+
+  const navigateToProfile = useCallback(() => {
+    // navigate('Profile');
+    signOut();
+  }, [navigate]);
 
   return (
     <Container>
-      <Button title="sair" onPress={signOut} />
+      <Header>
+        <HeaderTitle>
+          Bem vindo, {'\n'}
+          <UserName>{user.name}</UserName>
+        </HeaderTitle>
+
+        <ProfileButton onPress={navigateToProfile}>
+          <UserAvatar source={{ uri: user.avatar_url }} />
+        </ProfileButton>
+      </Header>
+
+      <ProvidersList
+        data={providers}
+        keyExtractor={provider => provider.id}
+        renderItem={({ item }) => <UserName>{item.name}</UserName>}
+      />
     </Container>
   );
 };
